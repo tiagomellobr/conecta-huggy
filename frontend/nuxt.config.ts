c924f6d3643b550d62e55e1562103413ad0a8f83
Tiago Mello <tiagomellobr@gmail.com>
2025-02-03 13:17:34 -0300

    feat: adiciona suporte ao Google Analytics com nuxt-gtag

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,11 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate/nuxt',
+    'nuxt-gtag',
+  ],
   buildModules: ["@nuxtjs/axios"],
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.API_BASE_URL
+      apiBaseUrl: process.env.API_BASE_URL,
     }
-  }
+  },
+  gtag: {
+    enabled: process.env.NODE_ENV === 'production',
+    id: 'G-XXXXXXXXXX'
+  },
 })
