b9d60c392cee68ede305d0d5edfd0efd819a972d
Tiago Mello <tiagomellobr@gmail.com>
2025-02-03 15:36:52 -0300

    fix: remove script

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -53,4 +53,4 @@
             }
         ]
     });
-</script>var $_Huggy = { defaultCountry: '+55', uuid: '5741a2cd-8d98-4fb6-b598-767d8c7c13d1' , company: '359123' }; (function(i,s,o,g,r,a,m){ i[r]={context:{id:'3c2b78346489fcd071c5fb12bf393aab'}};a=o;o=s.createElement(o); o.async=1;o.src=g;m=s.getElementsByTagName(a)[0];m.parentNode.insertBefore(o,m); })(window,document,'script','https://js.huggy.chat/widget.min.js','pwz');
+</script>
